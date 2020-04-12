fe04b34180aaf882db9fa2f3525e1d08666c8d69
SahilKr24 <io.sahilkr@gmail.com>
2020-04-12 15:49:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    path = require('path');
+
+app.use(bodyParser.json())          // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }))
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/',(req,res)=>{
+    res.redirect('/index')
+})
+app.get('/index', (req, res) => {
+    res.render('index.ejs')
+})
+
+var port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server live at port: ${port}`)
+})
